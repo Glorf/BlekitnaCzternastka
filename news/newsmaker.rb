487be3b5687216e2b2c70c1e52e3476c443e153c
Ian Sowinski <sowinj@gmail.com>
2017-03-01 00:21:29 +0100

    script working

--- news/newsmaker.rb
+++ news/newsmaker.rb
@@ -8,21 +8,15 @@ def get (uri)
   return Net::HTTP.get_response(URI(uri)).body
 end
 
-#uri = URI('http://example.com/index.html?count=10')
 $acces_token = get("https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id=#{$app_id}&client_secret=#{$app_secret}")
 
 data = get("https://graph.facebook.com/blekitna14/posts?#{$acces_token}&limit=100")
-
-#puts data
-
 array = []
 
 items = JSON.parse(data)['data']
 items.each_with_index do |item, index|
   array << {:text => item['message'], :date => item['created_time']}
-  #puts item['id']
   attachments= get("https://graph.facebook.com/#{item['id']}/attachments?#{$acces_token}")
-  #puts attachments
   parsed_attachment = JSON.parse(attachments)['data']
   next if parsed_attachment[0] == nil
   if parsed_attachment[0]['media'] != nil
@@ -31,7 +25,13 @@ items.each_with_index do |item, index|
     array[index][:img] = parsed_attachment[0]['subatachments']['data'][0]['media']['image']['src']
   end
 end
-array.each {|i| puts i[:img]}
-  #attachments_parsed = JSON.parse(attachments)['data']
-  #if attachments_parsed[0].media = nil
-  #  img_src = 
+
+html_array = []
+
+array.each do |item| 
+  html_array << "<div class=\"entry\"><img src=\"#{item[:img]}\"/><p>#{item[:text]}</p><p>#{item[:date]}</p></div>"
+end
+
+File.open('generated_index.html', 'w+') do |file|
+  file.puts "<!DOCTYPE html><html><head></head><body>#{html_array.join("")}</body></html"
+end
